759d6a1ddcbb3c0888ee6c806d003c2cb138f6ed
lillian-jewel <lillian.j.arcos@gmail.com>
2023-05-24 13:56:49 -0500

    Restarting

---
+++ src/test/java/com/codeup/codespringblog/CodeSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.codespringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
